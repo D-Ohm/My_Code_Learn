65756a8d3ea20fbd111cf13735dca928615030cd
D.Ohm <demian07@gmail.com>
2020-08-13 22:20:25 -0300

    Tkinter curse

---
+++ Tkinter I.py
@@ -0,0 +1,11 @@
+# Tkinter I
+
+from tkinter import *
+
+raiz = Tk()
+raiz.title("Ventana de prueba")
+raiz.resizable(False, False)
+raiz.geometry("650x350")
+raiz.config(bg="yellow")
+'''raiz.iconbitmap('~/Documents/Cursos/CursoPython/Imagenes Py/Iconos/snowflake-4-16.ico')'''
+raiz.mainloop()
